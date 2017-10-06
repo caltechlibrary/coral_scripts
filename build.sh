27c4c91c75e7693723a2a2c96c5cbe9347c68a95
Tommy Keswick <tkeswick@caltech.edu>
2017-10-06 10:18:18 -0700

    Require naming scheme for patch files; determine applicable branch.

--- build.sh
+++ build.sh
@@ -16,14 +16,27 @@ patches_dir="$project_root"/patches
 ## Stash all local changes, pull any remote changes, and then apply patches.
 # Change directory to the web root.
 cd "$web_root" || exit
+# todo: Make branch name a command argument.
+# todo: Checkout can be performed by this script even if there are conflicts.
+# Get the current branch name (indicated with `*`).
+branch=$( git branch | grep "*" | cut -d " " -f 2 )
 # Stash any local changes instead of deleting them.
 git stash
 # Pull the latest code from the current branch.
-# @TODO Define branch as a command argument.
 git pull
-# Get a list of `*.patch` files from the `patches` directory.
-# @TODO Apply patches based on `master` or `development` branch.
-find "$patches_dir" -type f -name "*.patch" | while read -r patch; do
+# Get a list of branch-specific `*.patch` files from the `patches` directory.
+find "$patches_dir" -type f -name "${branch}-??.*.patch" | while read -r patch; do
+    # Copy the current patch file to web root.
+    cp "$patch" .
+    # Get the file name of the patch file.
+    patch=$(basename "$patch")
+    # Apply the patch from the web root.
+    git apply -v "$patch"
+    # Remove the copy of the patch file in the web root.
+    rm "$patch"
+done
+# Get a list of branch-unspecific `*.patch` files from the `patches` directory.
+find "$patches_dir" -type f -name "all-??.*.patch" | while read -r patch; do
     # Copy the current patch file to web root.
     cp "$patch" .
     # Get the file name of the patch file.
